081e39bb9a2aa66de6c86513fa80dae11f806ee0
indranil <indranil@Pheonix>
2013-05-03 13:22:03 -0400

    after moding package

--- CourierWorld/src/sim/user/User.java
+++ CourierWorld/src/sim/user/User.java
@@ -8,31 +8,42 @@ import java.util.List;
 import sim.courier.Courier;
 import sim.courierworld.NodePackage;
 
+
 /**
- * Purpose of a user is to randomly generate NodePackage objects
- * for its node.  So each node contains a User.
+ * Purpose of a user is to randomly generate NodePackage objects for its node.
+ * So each node contains a User.
+ *
  * @author drew
  */
 public class User {
+
     private NodePackage nodePackage;
+    private int max_packages;
+    private long userID;
 
-    public User() {
+    public User(int max_packages, long userID) {
         nodePackage = new NodePackage();
-    }
-    
-    /**
-     * Each timestep a user 
-     */
-    public void generateNodePackage()
-    {
-        
+        this.max_packages = max_packages;
+        this.userID = userID;
     }
 
-    
-    public Courier chooseCourier(List<Courier> courier)
-    {
+    /**
+     * Each timestep a user generates a node package
+     */
+    public void generatePackages() {
+    }
+
+    public Courier chooseCourier(List<Courier> courier) {
         // call the step function on 
-        
+
         return null;
     }
+
+    /*public boolean acceptPackage(Packages packsDelivered) {
+
+        if (packsDelivered.userID == this.userID) {
+            return true;
+        }
+        return false;
+    }*/
 }
